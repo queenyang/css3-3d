67b6946fadff9288a6634d8dc1a940bcfc197fc3
sisterYang <838355402@qq.com>
2019-03-29 12:04:15 +0800

    index3  添加魔方前进后退按钮

--- js/index3.js
+++ js/index3.js
@@ -1,17 +1,23 @@
 window.onload = function() {
     // 加载页面 执行动画
     // 根据配置渲染banner分割个数，挪动图片位置
-    var bannerConDom = document.getElementsByClassName('banner-con')[0];
-    var wrpWidth = bannerConDom.offsetWidth;
-    init(4);
+    let iCurIndex = 0;
+    const part = 5;
+    const bannerConDom = document.getElementsByClassName('banner-con')[0];
+    const wrpWidth = bannerConDom.offsetWidth;
+    const prevBtn = document.getElementsByClassName('prev')[0];
+    const nextBtn = document.getElementsByClassName('next')[0];
+    init(part);
+    nextBtn.onclick = loopNext;
+    prevBtn.onclick = loopPrev;
     // 根据配置添加节点
     function init(part) {
-        var bannerInDom = '';
-        var itemWidth = parseInt(wrpWidth / part, 10) ;
-        var zIndexMax = Math.ceil(part / 2);
-        var remain = part % 2 ;
-        var zIndex = 0;
-        for(var i=0; i<part; i+=1) {
+        let bannerInDom = '';
+        let zIndex = 0;
+        const itemWidth = wrpWidth / part ;
+        const zIndexMax = Math.ceil(part / 2);
+        const remain = part % 2 ;
+        for(let i=0; i<part; i+=1) {
             if(i+1 <= zIndexMax) {
                 zIndex += 1;
             } else if( (i+1 <= zIndexMax + 1) && !remain ){
@@ -30,8 +36,27 @@ window.onload = function() {
         }
         bannerConDom.innerHTML = bannerInDom ;
     }
-    function loop() {
-
+    function loopNext() {
+        if(iCurIndex + 1 >= 4) {
+            iCurIndex = 0;
+        } else {
+            iCurIndex += 1;
+        }
+        turnOver();
     }
-
+    function loopPrev() {
+        if(iCurIndex - 1 <= 0) {
+            iCurIndex = 4;
+        } else {
+            iCurIndex -= 1;
+        }
+        turnOver();
+    }
+    // 魔方翻转动画
+    function turnOver() {
+        let bannerItemArr = document.getElementsByClassName('banner-item');
+        for(let i=0; i<bannerItemArr.length; i++) {
+            bannerItemArr[i].style.transform = `rotateX(${iCurIndex*90}deg)`;
+        }
+    }
 }
